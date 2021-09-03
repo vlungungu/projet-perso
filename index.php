27c9eece3f9964a8ea7171cc3dd839c182f9d6ac
victor lungungu <vlungungu@gmail.com>
2021-09-03 09:22:36 +0100

    create  index page

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page d'accueil</title>
+</head>
+<body>
+
+<h1>Bienvenue à la page d'accueil</h1>
+
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam asperiores delectus aliquam quasi voluptates veniam provident facere sint quisquam possimus, odit est praesentium tempora magni debitis, voluptate odio molestias doloribus!</p>
+
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, quos facere distinctio delectus alias rem id eum sequi saepe ut sunt ea mollitia atque dolores eos ex nulla! Nulla, minus?</p>
+    
+</body>
+</html>
